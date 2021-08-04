a763a7a32c3cc92cb8f849705f4563523a312972
liutianjiao <liutianjiao@changingedu.com>
2021-08-04 10:28:01 +0800

    feat: 增加项目名称等属性可配置功能

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ### create playground project cli
+
+配置了webpack5的模板下载命令行工具
+练习新技能时使用。
+希望你变强，但是不变秃。
+
+```bash
+npm install -g ltj-cli # 全局安装命令行工具
+```
+
+```bash
+ltj create <projectName>  #创建指定名称的练习项目
+```
--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,18 @@ const download = require('download-git-repo');
 const ora = require('ora');
 const chalk = require('chalk');
 const logSymbols = require('log-symbols');
+const shell = require('shelljs');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const handlebars = require('handlebars');
+const path = require('path');
 
 program.version('0.1.0');
 
 program
   .command('create <project>')
   .description('初始化项目模板')
-  .action(project => {
+  .action(projectName => {
     // https://www.npmjs.com/package/ora
     const spinner = ora('正在下载模板中...').start();
 
@@ -19,14 +24,51 @@ program
     // download的第一个参数是仓库地址#分支
     // 第二个参数是项目名称
     const downloadUrl = 'liutianjiao123/template';
-    download(downloadUrl, project, err => {
+    download(downloadUrl, projectName, err => {
       if (err) {
-        console.log('err : >>', err);
         spinner.fail();
         return console.log(logSymbols.error, chalk.red('下载模板失败:' + err));
       } else {
         spinner.succeed();
-        return console.log(logSymbols.success, chalk.green('下载模板成功'));
+        console.log(logSymbols.success, chalk.green('下载模板成功'));
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: '请输入项目名称',
+              default: projectName,
+            },
+            {
+              type: 'input',
+              name: 'description',
+              message: '请输入项目简介',
+              default: '',
+            },
+            {
+              type: 'input',
+              name: 'author',
+              message: '请输入作者名称',
+              default: '',
+            },
+          ])
+          .then(answers => {
+            //根据命令行答询结果修改package.json文件
+            let packageContent = fs.readFileSync(
+              `${projectName}/package.json`,
+              'utf8'
+            );
+            let packageResult = handlebars.compile(packageContent)(answers);
+            fs.writeFileSync(`${projectName}/package.json`, packageResult);
+            //用chalk和log-symbols改变命令行输出样式
+            console.log(logSymbols.success, chalk.green('模板配置成功'));
+          })
+          .catch(err => {
+            console.log(
+              logSymbols.error,
+              chalk.green('模板项目加载失败' + err)
+            );
+          });
       }
     });
   });
